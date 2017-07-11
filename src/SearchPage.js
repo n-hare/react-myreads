a49e7ae502ac87bad4bf4ea7a345f7671b840a5d
Nathan Hare <n.hare@hotmail.com>
2017-07-11 00:23:46 -0400

    refactor: Split to seperate components ListBooks and SearchPage

---
+++ src/SearchPage.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+class SearchPage extends React.Component {
+    render() {
+        return (
+            <div className="search-books">
+              <div className="search-books-bar">
+                <a className="close-search" href="#">Close</a>
+                <div className="search-books-input-wrapper">
+                  <input type="text" placeholder="Search by title or author"/>
+                </div>
+              </div>
+              <div className="search-books-results">
+                <ol className="books-grid"></ol>
+              </div>
+            </div>
+        )
+    }
+}
+
+export default SearchPage
