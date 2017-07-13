64b8614b7682996670774edb6ce504dae9224846
Nathan Hare <n.hare@hotmail.com>
2017-07-13 01:33:49 -0400

    refactor: Lift state to BookApp Component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Route } from 'react-router-dom'
+import * as BooksAPI from './BooksAPI'
 import './App.css'
 
 // Components
@@ -8,12 +9,38 @@ import SearchPage from './SearchPage'
 
 class BooksApp extends React.Component {
 
+  state= {
+        books: []
+    }
+
+    _handleShelfChange = (updateBook, evt) => {
+        const newShelf = evt.target.value
+        BooksAPI.update(updateBook, newShelf)
+            .then(() => {
+                const books = [...this.state.books]
+                books[books.indexOf(updateBook)].shelf = newShelf
+                this.setState({ books })
+
+            })
+
+    }
+
+    _updateBooks = (books) => {
+      this.setState({ books })
+    }
+
   render() {
     return (
-      <div className="app">
-        <Route exact path="/" component={ListBooks} />
-        <Route path="/search" component={SearchPage} />
-      </div>
+        <div className="app">
+            <Route exact path="/" render={ ()=>(
+                <ListBooks
+                    books={this.state.books}
+                    onShelfChange={ this._handleShelfChange }
+                    onUpdateBooks={this._updateBooks}
+                />
+            )} />
+            <Route path="/search" component={SearchPage} />
+        </div>
     )
   }
 }
--- src/ListBooks.js
+++ src/ListBooks.js
@@ -6,34 +6,19 @@ import * as BooksAPI from './BooksAPI'
 import BookShelf from './BookShelf'
 
 class ListBooks extends React.Component {
-    state= {
-        books: []
-    }
-
-    _handleShelfChange = (updateBook, evt) => {
-        const newShelf = evt.target.value
-        BooksAPI.update(updateBook, newShelf)
-            .then(() => {
-                const books = [...this.state.books]
-                books[books.indexOf(updateBook)].shelf = newShelf
-                this.setState({ books })
-
-            })
-
-    }
 
    componentDidMount() {
         BooksAPI.getAll()
             .then((books) => {
-                this.setState({ books })
+                this.props.onUpdateBooks( books )
             })
 
     }
 
     render() {
-        const currentlyReading =  this.state.books.filter((book)=> (book.shelf === 'currentlyReading')) || []
-        const wantToRead = this.state.books.filter((book)=> (book.shelf === 'wantToRead')) || []
-        const read = this.state.books.filter((book)=> (book.shelf === 'read')) || []
+        const currentlyReading =  this.props.books.filter((book)=> (book.shelf === 'currentlyReading')) || []
+        const wantToRead = this.props.books.filter((book)=> (book.shelf === 'wantToRead')) || []
+        const read = this.props.books.filter((book)=> (book.shelf === 'read')) || []
 
         return (
             <div className="list-books">
@@ -42,9 +27,9 @@ class ListBooks extends React.Component {
                 </div>
                 <div className="list-books-content">
                     <div>
-                        <BookShelf shelfTitle='Currently Reading' shelfBooks={ currentlyReading } onShelfChange={ this._handleShelfChange } />
-                        <BookShelf shelfTitle='Want to Read' shelfBooks={ wantToRead } onShelfChange={ this._handleShelfChange } />
-                        <BookShelf shelfTitle='Read' shelfBooks={ read } onShelfChange={ this._handleShelfChange } />
+                        <BookShelf shelfTitle='Currently Reading' shelfBooks={ currentlyReading } onShelfChange={ this.props.onShelfChange } />
+                        <BookShelf shelfTitle='Want to Read' shelfBooks={ wantToRead } onShelfChange={ this.props.onShelfChange } />
+                        <BookShelf shelfTitle='Read' shelfBooks={ read } onShelfChange={ this.props.onShelfChange } />
                     </div>
                 </div>
                 <div className="open-search">
